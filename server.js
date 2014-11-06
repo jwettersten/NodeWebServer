800ca05bdfe3438ccc6fe7acfac959d38e26f7c8
Jon Wettersten <jwettersten@ideo.com>
2014-11-06 11:16:38 -0600

    refactored and added echo test

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var net = require('net');
 
+var PORT = 8080;
+
 var TCPServer = new net.createServer(function(connListener) {
+  var responseContent = 'HTTP/1.1 200 OK' + '\r\n';
   console.log('server connected');
 
   connListener.on('end', function() {
     console.log('server disconnected');
   });
 
-  connListener.write('hey dude.');
-  connListener.pipe(connListener);
+  connListener.on('data', function(data) {
+    console.log('data received from client: ' + data);
+    // echo back to client
+    connListener.write(data);
+  });
 
+  connListener.write(responseContent);
 });
 
-TCPServer.listen(8080, function() {
+TCPServer.listen(PORT, function() {
   console.log('server has been bound and now listening!');
 });
--- spec/server-spec.js
+++ spec/server-spec.js
@@ -1,19 +1,38 @@
 var net = require('net');
 var testServer = require('../server.js');
 
+var PORT = 8080;
+var testHeaderOK = 'HTTP/1.1 200 OK' + '\r\n';
+
 describe('node TCPServer', function() {
 
   it('should return a TCPServer instance', function() {
     expect(testServer).not.toBe(undefined);
   });
 
-  it('should return hey dude.', function() {
-    var testClient = net.connect({port: 8080}, function() {});
+  it('should return 200 header data', function() {
+
+    var testClient = net.connect({port: PORT}, function() {});
 
     waits(1000);
 
     testClient.on('data', function(data) {
-      expect(data.toString()).toBe('hey dude.');
+      expect(data.toString()).toBe(testHeaderOK);
+      testClient.end();
+    });
+  });
+
+  it('should echo data sent', function() {
+    var testMessage = 'Hey TCPServer';
+
+    var testClient = net.connect({port: PORT}, function() {
+      testClient.write(testMessage);
+    });
+
+    waits(1000);
+
+    testClient.on('data', function(data) {
+      expect(data.toString()).toBe(testHeaderOK + testMessage);
       testClient.end();
     });
   });
