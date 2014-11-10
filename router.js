862cf41ecfc7d03b0acadc24312987e6b222ef7a
Jon Wettersten <jwettersten@ideo.com>
2014-11-10 13:55:16 -0600

    working through reading and prefixing headers

--- router.js
+++ router.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
+var Readable = require('stream').Readable;
+var util = require('util');
 
 function route(pathData, connListener) {
   var responseContent = 'HTTP/1.1 200 OK' + '\r\n';
+  var responseFileHeader = 'Content-Type: text/html' + '\r\n' + 'Content-Length: 35' + '\r\n';
 
   var clientData = pathData.toString().split(' ');
 
@@ -9,12 +12,20 @@ function route(pathData, connListener) {
   if (clientData[0] === 'GET') {
     if (clientData[1] === '/file1') {
       console.log('GET request for: ' + clientData[1]);
+
+      var rs = new Readable();
+      rs.push(responseContent);
+      rs.push(responseFileHeader);
+
       var fileStream = fs.createReadStream('./public' + clientData[1]);
-      fileStream.pipe(connListener);
+
       fileStream.on('end', function() {
         console.log('stream has ended');
         connListener.end();
       });
+
+      fileStream.pipe(connListener);
+
     } else if (clientData[1] === '/image.jpeg') {
       var fileStream = fs.createReadStream('./public' + clientData[1]);
       fileStream.pipe(connListener);
